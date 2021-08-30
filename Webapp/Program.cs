2cfb0955867222adb4763514a4cb801470cf2671
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-08-30 09:05:42 +0200

    inhämtning från uppgift 1

--- Webapp/Program.cs
+++ Webapp/Program.cs
@@ -11,16 +11,16 @@ namespace Webapp
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public static void Main(string[] args)
+	{
+	    CreateHostBuilder(args).Build().Run();
+	}
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+	public static IHostBuilder CreateHostBuilder(string[] args) =>
+	    Host.CreateDefaultBuilder(args)
+		.ConfigureWebHostDefaults(webBuilder =>
+		{
+		    webBuilder.UseStartup<Startup>();
+		});
     }
 }
