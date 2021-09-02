2249d211f4de56855af6da5a55129e33b958dc44
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-09-02 16:54:33 +0200

    modellklass som står för utlåtandet om feber/hypotermi elle inte alls

--- Webapp/Modeller/Doktorn.cs
+++ Webapp/Modeller/Doktorn.cs
@@ -1,33 +1,14 @@
 namespace Webapp.Modeller {
-    public class Doktorn {
-	public Doktorn() {
-	}
-
-	public static  Utlåtande(float temp) {
-	    if (temp.Kroppstemp >= 38 ) {
-		Console.WriteLine( "Du har feber");
-		ViewData["status"]= "Du har feber";
-	    } else if (temp.Kroppstemp > 37.2 && temp.Kroppstemp < 38 ) {
-		Console.WriteLine( "Du kanske har lite feber");
-		ViewData["status"]= "Du kanske har lite feber";
-	    } else if (temp.Kroppstemp < 36.2 ) {
-		Console.WriteLine( "Du HAR hypotermi");
-		ViewData["status"]= "Du HAR hypotermi";
-	    } else {
-		Console.WriteLine( "Du har inte någon feber, ja du har faktiskt en normal temperatur");
-		ViewData["status"]= "Din kroppstemperatur är helt normal";
-	    }
-	}
-
+    public class DoktorBoström {
 	public static string Utlåtande(float temp) {
-	    if (temp >= 38)
+	    if (temp >= 38.0)
 		return "Du har feber";
-	    else if (temp.Kroppstemp > 37.2 && temp.Kroppstemp < 38 )
+	    else if (temp > 37.2 && temp < 38 )
 		return "Du kanske har lite feber";
-	    else if (temp.Kroppstemp < 36.2 )
-		return "Du HAR hypotermi"
-		else
-		    return "Din kroppstemperatur är helt normal";
+	    else if (temp < 36.0 )
+		return "Du HAR hypotermi";
+	    else
+		return "Din kroppstemperatur är helt normal";
 	}
     }
 }
