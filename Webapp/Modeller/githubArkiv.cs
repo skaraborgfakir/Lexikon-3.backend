9d3b893bbe21ef15fa4479cbc6362e99573d6914
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-09-03 12:02:57 +0200

    flytta in githubarkiv in i modellerna !!

--- Webapp/Modeller/githubArkiv.cs
+++ Webapp/Modeller/githubArkiv.cs
@@ -1,4 +1,5 @@
-// Time-stamp: <2021-08-31 12:56:12 stefan>
+// Time-stamp: <2021-09-03 11:56:03 stefan>
+//
 
 using System;
 using System.Net;
@@ -6,34 +7,36 @@ using System.IO;
 
 // using JQ;
 
-public class githubArkiv {
-    public string result;
+namespace Webapp.Modeller {
+    public class githubArkiv {
+	public string result;
 
-    public string Result {
-	get { return result; }
-	private set { result = value; }
-    }
-
-    public githubArkiv() {
-	// https://reqbin.com/req/csharp/c-vdhoummp/curl-get-json-example:
-	var url = "https://api.github.com/users/Skaraborgfakir/repos";
-	var httpRequest = (HttpWebRequest) WebRequest.Create(url);
-
-	httpRequest.Accept = "application/json";
-	// api.github.com kräver att användaren anger UserAgent, men...
-	httpRequest.UserAgent = "Mosaic3/Kronos executive"; // fungerar det ? Konstig CYBER maskin
-	// det fungerar .... alltså kan man hitta på ganska så fritt.... :-)
-	Console.WriteLine("\nThe HttpHeaders are \n{0}",httpRequest.Headers);
-
-	//
-	// sug ner strukturen
-	//
-	var httpResponse = (HttpWebResponse) httpRequest.GetResponse();
-
-	using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
-	{
-	    Result = streamReader.ReadToEnd();
+	public string Result {
+	    get { return result; }
+	    private set { result = value; }
 	}
-    }
 
+	public githubArkiv() {
+	    // https://reqbin.com/req/csharp/c-vdhoummp/curl-get-json-example:
+	    var url = "https://api.github.com/users/Skaraborgfakir/repos";
+	    var httpRequest = (HttpWebRequest) WebRequest.Create(url);
+
+	    httpRequest.Accept = "application/json";
+	    // api.github.com kräver att användaren anger UserAgent, men...
+	    httpRequest.UserAgent = "Mosaic3/Kronos executive"; // fungerar det ? Konstig CYBER maskin
+	    // det fungerar .... alltså kan man hitta på ganska så fritt.... :-)
+	    Console.WriteLine("\nThe HttpHeaders are \n{0}",httpRequest.Headers);
+
+	    //
+	    // sug ner strukturen
+	    //
+	    var httpResponse = (HttpWebResponse) httpRequest.GetResponse();
+
+	    using (var streamReader = new StreamReader(httpResponse.GetResponseStream()))
+	    {
+		Result = streamReader.ReadToEnd();
+	    }
+	}
+
+    }
 }
