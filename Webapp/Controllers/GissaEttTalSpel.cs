a8cd6e18095c6fc6e4cd99b81f945712717d0743
Stefan Niskanen Skoglund <stefan.skoglund@agj.net>
2021-09-03 14:03:32 +0200

    tar med logger i kontrollanten

--- Webapp/Controllers/GissaEttTalSpel.cs
+++ Webapp/Controllers/GissaEttTalSpel.cs
@@ -1,4 +1,4 @@
-// Time-stamp: <2021-09-02 15:04:12 stefan>
+// Time-stamp: <2021-09-03 14:01:54 stefan>
 
 using System;
 // using System.Collections.Generic;
@@ -17,20 +17,65 @@ using Microsoft.AspNetCore.Mvc;
 // https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.logging?view=dotnet-plat-ext-3.1
 using Microsoft.Extensions.Logging;
 
+// https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.http?view=aspnetcore-3.1
 using Microsoft.AspNetCore.Http;
+
+// https://docs.microsoft.com/en-us/dotnet/api/microsoft.extensions.configuration?view=dotnet-plat-ext-3.1
+using Microsoft.Extensions.Configuration; // IConfiguration
 
 using Webapp.Vydata;
 using Webapp.Modeller;
 using Webapp.Hjälpklasser;
 
+// public class TodoController : ControllerBase
+// {
+//     private readonly ILogger _logger;
+
+//     public TodoController(ILogger<TodoController> logger)
+//     {
+//         _logger = logger;
+//     }
+
+//     [HttpGet("{id}", Name = "GetTodo")]
+//     public ActionResult<TodoItem> GetById(string id)
+//     {
+//         _logger.LogInformation(LoggingEvents.GetItem, "Getting item {Id}", id);
+
+//         // Item lookup code removed.
+
+//         if (item == null)
+//         {
+//             _logger.LogWarning(LoggingEvents.GetItemNotFound, "GetById({Id}) NOT FOUND", id);
+//             return NotFound();
+//         }
+
+//         return item;
+//     }
+// }
+
 namespace Webapp.Controllers
 {
     public class GissaEttTalSpel : Controller {
-	public GissaEttTalSpel() {
+    private readonly ILogger _logger;
+
+	public GissaEttTalSpel(ILogger<TodoController> logger) {
+	    _logger = logger;
 	}
 
 	//
 	// GET:
+	//
+	// code requirements:
+	//   1  The guessing game View should be accessible through a custom route, using the
+	//       “/GuessingGame” pattern, regardless of what the controller is named.
+	//
+	//   2  The random number should be stored in the Session state.
+	//
+	//   3  Guessing should be handled through overloaded Actions, referring to the same View.
+	//      - When the page is loaded through a GET request (such as through the URL), the
+	//        app should reset, and start over with a new number.
+	//      - When the page is loaded through a POST request, it should make a guess, unless
+	//        the value isn’t provided or invalid, in which case it should display an error message.
 	//
 	// https://www.infoworld.com/article/3600190/how-to-overload-action-methods-in-aspnet-core-mvc-5.html
 	//
